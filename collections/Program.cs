67417865384b6b167de54409edcf752ebab0f328
muralimd07 <60916429+muralimd07@users.noreply.github.com>
2020-12-23 01:07:14 -0800

    list pgm added

--- collections/Program.cs
+++ collections/Program.cs
@@ -10,6 +10,35 @@ namespace collections
     {
         static void Main(string[] args)
         {
+            List<string> m1 = new List<string>();
+            m1.Add("murali");
+            m1.Add("kumar");
+
+            //for sorting
+            //m1.Sort();
+
+            //insert at place
+            m1.Insert(2, "ad");
+            string x = "";
+            foreach (string person in m1)
+            {
+                x += person + "\n";
+            }
+
+            Console.WriteLine(x);
+
+            m1.Remove("ad");
+            if (m1.Contains("anand"))
+            {
+                Console.WriteLine("found");
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+
+            Console.ReadKey();
         }
+
     }
 }
